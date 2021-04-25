61cc28b7978e29d1936aab1b695f71f8db9258ea
Rafael Oliveira <70673133+rafinhao11@users.noreply.github.com>
2021-04-25 16:47:01 -0300

    Add files via upload

---
+++ exercise2.py
@@ -0,0 +1,4 @@
+hrs = input('Hours: ')
+rate = input('Rate: ')
+salary = float(hrs) * float(rate)
+print('Salary: ', salary)
---
+++ exercise3.py
@@ -0,0 +1,9 @@
+hrs = input("Enter Hours:")
+h = float(hrs)
+rate = input("Enter Hours:")
+r = float(rate)
+if h <= 40:
+    h = h * r
+else:
+        h = 40 * r + (h - 40) * r * 1.5
+print(h)
